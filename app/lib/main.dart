a5826f78795a502160e129019ca3aa49df4b8247
Jonas Greifenhain <cadivus@daverkomp.de>
2022-12-17 08:53:58 +0100

    app: Use `CosinussSensor`

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:cosinuss_lib/cosinuss_sensor.dart';
+import 'package:cosinuss_lib/data_model/cosinuss_data.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,17 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Cosinuss Sensor Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Cosinuss° One - Demo'),
@@ -32,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,96 +32,91 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _connectionStatus = "Disconnected";
-  String _heartRate = "- bpm";
-  String _bodyTemperature = '- °C';
-
-  String _accX = "-";
-  String _accY = "-";
-  String _accZ = "-";
-
-  String _ppgGreen = "-";
-  String _ppgRed = "-";
-  String _ppgAmbient = "-";
-
-  bool _isConnected = false;
-
-  bool earConnectFound = false;
+  bool _connected = false;
 
   @override
   Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Padding(
           padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(children: [
-                const Text(
-                  'Status: ',
-                ),
-                Text(_connectionStatus),
-              ]),
-              Row(children: [
-                const Text('Heart Rate: '),
-                Text(_heartRate),
-              ]),
-              Row(children: [
-                const Text('Body Temperature: '),
-                Text(_bodyTemperature),
-              ]),
-              Row(children: [
-                const Text('Accelerometer X: '),
-                Text(_accX),
-              ]),
-              Row(children: [
-                const Text('Accelerometer Y: '),
-                Text(_accY),
-              ]),
-              Row(children: [
-                const Text('Accelerometer Z: '),
-                Text(_accZ),
-              ]),
-              Row(children: [
-                const Text('PPG Raw Red: '),
-                Text(_ppgRed),
-              ]),
-              Row(children: [
-                const Text('PPG Raw Green: '),
-                Text(_ppgGreen),
-              ]),
-              Row(children: [
-                const Text('PPG Ambient: '),
-                Text(_ppgAmbient),
-              ]),
-              Row(children: const [
-                Text(
-                    '\nNote: You have to insert the earbud in your  \n ear in order to receive heart rate values.')
-              ]),
-              Row(children: const [
-                Text(
-                    '\nNote: Accelerometer and PPG have unknown units. \n They were reverse engineered. \n Use with caution!')
-              ]),
-            ],
+          child: StreamBuilder<CosinussData>(
+            stream: CosinussSensor.instance.stream,
+            builder: (
+                BuildContext context,
+                AsyncSnapshot<CosinussData> snapshot,
+                ) {
+              if (!snapshot.hasData) {
+                return _connected ? const CircularProgressIndicator() : const Text("Disconnected");
+              }
+
+              _connected = snapshot.data!.connected;
+
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(children: [
+                    const Text(
+                      'Status: ',
+                    ),
+                    Text(CosinussSensor.instance.isConnected ? "Connected" : "Disconnected"),
+                  ]),
+                  Row(children: [
+                    const Text('Heart Rate: '),
+                    Text(snapshot.data!.heartRate.toString()),
+                  ]),
+                  Row(children: [
+                    const Text('Body Temperature: '),
+                    Text(snapshot.data!.bodyTemperature.toString()),
+                  ]),
+                  Row(children: [
+                    const Text('Accelerometer X: '),
+                    Text((snapshot.data!.accelerometer?.x ?? "-").toString()),
+                  ]),
+                  Row(children: [
+                    const Text('Accelerometer Y: '),
+                    Text((snapshot.data!.accelerometer?.y ?? "-").toString()),
+                  ]),
+                  Row(children: [
+                    const Text('Accelerometer Z: '),
+                    Text((snapshot.data!.accelerometer?.z ?? "-").toString()),
+                  ]),
+                  Row(children: [
+                    const Text('PPG Raw Red: '),
+                    Text((snapshot.data!.ppgRaw?.ppgRed ?? "-").toString()),
+                  ]),
+                  Row(children: [
+                    const Text('PPG Raw Green: '),
+                    Text((snapshot.data!.ppgRaw?.ppgGreen ?? "-").toString()),
+                  ]),
+                  Row(children: [
+                    const Text('PPG Ambient: '),
+                    Text((snapshot.data!.ppgRaw?.ppgAmbient ?? "-").toString()),
+                  ]),
+                  Row(children: const [
+                    Text(
+                        '\nNote: You have to insert the earbud in your  \n ear in order to receive heart rate values.')
+                  ]),
+                  Row(children: const [
+                    Text(
+                        '\nNote: Accelerometer and PPG have unknown units. \n They were reverse engineered. \n Use with caution!')
+                  ]),
+                ],
+              );
+            },
           ),
         ),
       ),
       floatingActionButton: Visibility(
-        visible: !_isConnected,
+        visible: !CosinussSensor.instance.isConnected,
         child: FloatingActionButton(
-          onPressed: () {},
-          tooltip: 'Increment',
+          onPressed: () {
+            CosinussSensor.instance.connect();
+          },
+          tooltip: 'Connect',
           child: const Icon(Icons.bluetooth_searching_sharp),
         ),
       ),
